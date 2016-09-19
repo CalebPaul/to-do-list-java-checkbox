1b78b9561e445b135cc317cc12f9c3a805ba2538
Jackson Meyer and Sandro Alvarez <jackson+sandro@>
2016-09-19 13:56:54 -0700

    added all passing tests for setting up Database

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -1,5 +1,9 @@
+import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public class CategoryTest {
   private Category firstCategory;
@@ -7,10 +11,10 @@ public class CategoryTest {
   private Task testTask;
   @Before
   public void initialize() {
-    Category.clear();
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
     firstCategory = new Category("Inside");
     secondCategory = new Category("Outside");
-    testTask = new Task("Make bed");
+    testTask = new Task("Make bed", 1);
   }
 
   @Test
@@ -24,30 +28,63 @@ public class CategoryTest {
   }
 
   @Test
+  public void equals_returnsTrueIfNamesAreTheSame() {
+    Category testCategory = new Category("Inside");
+    assertTrue(firstCategory.equals(testCategory));
+  }
+
+  @Test
   public void all_checksIfCategoryListContainsInstance_true() {
-    assertTrue(Category.all().contains(firstCategory));
-    assertTrue(Category.all().contains(secondCategory));
+    firstCategory.save();
+    secondCategory.save();
+    assertTrue(Category.all().get(0).equals(firstCategory));
+    assertTrue(Category.all().get(1).equals(secondCategory));
    }
 
   @Test
   public void getID_returnsIDNumber_int() {
-    assertEquals(1, firstCategory.getID());
+    firstCategory.save();
+    assertTrue(firstCategory.getId() > 0);
   }
 
   @Test
-  public void clear_clearsCategoryList_0() {
-    Category.clear();
-    assertEquals(Category.all().size(), 0);
+  public void getTasks_retrievesAllTasksFromDataBase_tasksList() {
+    firstCategory.save();
+    Task firstTask = new Task("Mow the lawn", firstCategory.getId());
+    firstTask.save();
+    Task secondTask = new Task("Do the dishes", firstCategory.getId());
+    secondTask.save();
+    Task[] tasks = new Task[] { firstTask, secondTask };
+    assertTrue(firstCategory.getTasks().containsAll(Arrays.asList(tasks)));
   }
 
   @Test
-  public void addTask_addsTasksToCategoryList_true() {
-    firstCategory.addTask(testTask);
-    assertTrue(firstCategory.getTasks().contains(testTask));
+  public void find_returnsCategoryWithSameId_secondCategory() {
+    firstCategory.save();
+    secondCategory.save();
+    assertEquals(Category.find(secondCategory.getId()), secondCategory);
   }
 
   @Test
-  public void findCategory_returnsCategoryWithSameID_secondCategory() {
-    assertEquals(Category.find(secondCategory.getID()), secondCategory);
+  public void save_assignsIdToObject() {
+    firstCategory.save();
+    Category savedCategory = Category.all().get(0);
+    assertEquals(firstCategory.getId(), savedCategory.getId());
+  }
+
+  @Test
+  public void save_savesIntoDatabase_true() {
+    firstCategory.save();
+    assertTrue(Category.all().get(0).equals(firstCategory));
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteTasksQuery = "DELETE FROM tasks *;";
+      String deleteCategoriesQuery = "DELETE FROM categories *;";
+      con.createQuery(deleteTasksQuery).executeUpdate();
+      con.createQuery(deleteCategoriesQuery).executeUpdate();
+    }
   }
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,6 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.time.LocalDateTime;
+import org.sql2o.*;
 
 public class TaskTest {
   private Task firstTask;
@@ -8,9 +9,9 @@ public class TaskTest {
 
   @Before
   public void initialize() {
-    Task.clear();
-    firstTask = new Task("Mow the lawn");
-    secondTask = new Task("Fly to Mars");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
+    firstTask = new Task("Mow the lawn", 1);
+    secondTask = new Task("Fly to Mars", 1);
   }
 
   @Test
@@ -35,23 +36,55 @@ public class TaskTest {
 
   @Test
   public void all_returnsAllInstancesOfTask_true() {
-    assertTrue(Task.all().contains(firstTask));
-    assertTrue(Task.all().contains(secondTask));
+    firstTask.save();
+    secondTask.save();
+    assertTrue(Task.all().get(0).equals(firstTask));
+    assertTrue(Task.all().get(1).equals(secondTask));
   }
 
   @Test
-  public void clear_emptiesAllTasksFromArrayList_0() {
-    Task.clear();
-    assertEquals(Task.all().size(), 0);
-  }
-
-  @Test
-  public void getId_tasksInstantiateWithAnID_1() {
-    assertEquals(1, firstTask.getID());
+  public void getId_tasksInstantiateWithAnId_1() {
+    firstTask.save();
+    assertTrue(firstTask.getId() > 0);
   }
 
   @Test
   public void find_returnsTaskWithSameId_secondTask() {
-    assertEquals(Task.find(secondTask.getID()), secondTask);
+    firstTask.save();
+    secondTask.save();
+    assertEquals(Task.find(secondTask.getId()), secondTask);
+  }
+
+  @Test
+  public void equals_returnsTrueIfDescriptionsAreTheSame() {
+    Task testTask = new Task("Mow the lawn", 1);
+    assertTrue(firstTask.equals(testTask));
+  }
+
+  @Test
+  public void save_returnsTrueIfDescriptionsAretheSame() {
+    Task myTask = new Task("Mow the lawn", 1);
+    myTask.save();
+    Task savedTask = Task.all().get(0);
+    assertTrue(Task.all().get(0).equals(myTask));
+    assertEquals(myTask.getId(), savedTask.getId());
+  }
+
+  @Test
+  public void save_savesCategoryIdIntoDB_true() {
+    Category myCategory = new Category("chores");
+    myCategory.save();
+    Task myTask = new Task("Mow the lawn", myCategory.getId());
+    myTask.save();
+    Task savedTask = Task.find(myTask.getId());
+    assertEquals(savedTask.getCategoryId(), myCategory.getId());
+  }
+
+  @After
+    public void tearDown() {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "DELETE FROM tasks *;";
+        con.createQuery(sql).executeUpdate();
+    }
   }
 }
